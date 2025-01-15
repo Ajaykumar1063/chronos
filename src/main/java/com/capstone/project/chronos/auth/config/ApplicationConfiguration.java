7c739cfaac09620bb13b84f67c30f7390e8c0502
ajaykumarvala <ajay@socly.io>
2025-01-15 16:56:54 +0530

    Chronos final deployement

--- src/main/java/com/capstone/project/chronos/auth/config/ApplicationConfiguration.java
+++ src/main/java/com/capstone/project/chronos/auth/config/ApplicationConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class ApplicationConfiguration implements WebMvcConfigurer {
+public class ApplicationConfiguration {
 
   @Autowired
   private AuthenticationProvider authenticationProvider;
@@ -36,14 +36,5 @@ public class ApplicationConfiguration implements WebMvcConfigurer {
         .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
-  }
-
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**") // Apply this rule to all /api/** endpoints
-            .allowedOrigins("http://localhost:3000") // Allow requests from localhost:3000
-            .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
-            .allowedHeaders("*") // Allow any header
-            .allowCredentials(true); // Allow credentials (cookies, HTTP authentication)
   }
 }
--- src/main/java/com/capstone/project/chronos/jobscheduling/config/SchedulerConfig.java
+++ src/main/java/com/capstone/project/chronos/jobscheduling/config/SchedulerConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SchedulerConfig {
+public class SchedulerConfig implements WebMvcConfigurer {
     @Bean
     public TaskScheduler taskScheduler() {
         return new ConcurrentTaskScheduler(); // Simple TaskScheduler implementation
@@ -17,6 +19,15 @@ public class SchedulerConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Apply this rule to all /api/** endpoints
+                .allowedOrigins("http://localhost:3000") // Allow requests from localhost:3000
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
+                .allowedHeaders("*") // Allow any header
+                .allowCredentials(true); // Allow credentials (cookies, HTTP authentication)
     }
 }
 
